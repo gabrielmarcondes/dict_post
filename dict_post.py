547119e38c16541a36f64ba7f921f589670b5bce
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2016-07-20 17:01:19 -0300

    passo 5: get item, contains, values, items

--- dict_post.py
+++ dict_post.py
@@ -8,7 +8,23 @@ class MeuDicionario:
         posicao = hash(chave) % self._tamanho
         self._chaves[posicao] = chave
         self._valores[posicao] = valor
+    
+    def __getitem__(self, chave):
+        posicao = hash(chave) % self._tamanho
+        return self._valores[posicao]
+    
+    def __contains__(self, chave):
+        posicao = hash(chave) % self._tamanho
+        return self._chaves[posicao] is not None
+    
+    def values(self):
+        return [valor for valor in self._valores if valor is not None]
 
+    def items(self):
+        for par in zip(self._chaves, self._valores):
+            if par[0] is not None:
+                yield par
+        
 # testes
 d = MeuDicionario()
 d["chave"] = "valor"
@@ -18,4 +34,6 @@ assert "valor" in d.values()
 for c, v in d.items():
     assert c == "chave"
     assert v == "valor"
+
+print("sucesso!")
 
