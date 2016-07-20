a5a248556303ff9d1c25302fccf8e7dcbfda5f99
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2016-07-20 17:00:03 -0300

    passo 3: inicialização, set item

--- dict_post.py
+++ dict_post.py
@@ -1,8 +1,16 @@
-# código
 class MeuDicionario:
-    pass
+    def __init__(self):
+        self._tamanho = 128
+        self._chaves = [None for i in range(128)]
+        self._valores = [None for i in range(128)]
+    
+    def __setitem__(self, chave, valor):
+        posicao = hash(chave) % self._tamanho
+        self._chaves[posicao] = chave
+        self._valores[posicao] = valor
 
 # testes
 d = MeuDicionario()
 d["chave"] = "valor"
 assert d["chave"] == "valor"
+
